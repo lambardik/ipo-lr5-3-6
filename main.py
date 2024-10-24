b15fdf11d9ec28c6e66f0691573bab2668c04e4d
даниэль <dannilboyar@gmail.com>
2024-10-24 10:36:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-def create_unique_word_file(input_filename, output_filename):
-
-  with open(input_filename, 'r',encoding = 'utf-8') as input_file, open(output_filename, 'w',encoding='utf-8') as output_file:
-    words = set()
-    for line in input_file:
-      for word in line.split():
-        words.add(word.lower())  # Добавляем слово в множество, не учитывая регистр
-    for word in words:
-      output_file.write(word + '\n')  # Записываем уникальные слова в выходной файл
-
 input_filename = 'text.txt'
 output_filename = 'output.txt'
-create_unique_word_file(input_filename, output_filename)
+
+words = set()  
+
+with open(input_filename, 'r', encoding='utf-8') as input_file, open(output_filename, 'w', encoding='utf-8') as output_file:
+  for line in input_file:
+    for word in line.split():
+      words.add(word.lower())
+
+  for word in words:
+    output_file.write(word + '\n')
+
 print(f"Уникальные слова записаны в файл '{output_filename}'")
